b51d746477c1bc257b45af7bf3187ed5e3878a32
jmart0304 <jmartinez0304@tamu.edu>
2019-04-25 18:54:45 -0500

    Started deep q learning directory

--- DQN/agent.py
+++ DQN/agent.py
@@ -1,5 +1,9 @@
 import numpy as np
 import collections
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 """
 Contains the definition of the agent that will run in an
@@ -23,6 +27,8 @@ class RandomAgent:
         """
         self.learning_rate = .01   # learning rate 
         self.gamma = .8
+        self.obs_size = 9
+        self.action_size = 8
 
         self.epsilon_a = -1/900
         self.epsilon_b = 1
@@ -38,6 +44,22 @@ class RandomAgent:
         self.step = 0
 
         self.cumul_reward = 0
+        
+        self.memory = deque(maxlen=2000)
+        self.model = self._build_model()
+
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.obs_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
 
     def reset(self):
@@ -45,6 +67,7 @@ class RandomAgent:
         self.n_episode += 1
         if self.n_episode == 1000:
             print(self.cumul_reward)
+            print('Hmmm...')
             # print(self.q_table)
 
     def act(self, observation):
@@ -53,27 +76,38 @@ class RandomAgent:
 
         position, smell, breeze, charges = observation
 
+        observation_vector = [position[0], position[1], smell, breeze, charges]
+        for i in observation_vector:
+            if i == True:
+                i = 1
+            elif i == False:
+                i = 0
+
+
         print('position...', position)
         print('smell...', smell)
         print('breeze...', breeze)
         print('charges...', charges)
-        
+        print('observation_vector...', observation_vector)
+
         self.create_state(observation)
         state_action = self.q_table[observation]
-        print(self.q_table)
-        print(state_action)
+
         if self.n_episode >= 900:
+            print('Greater or equal to 900...')
             if smell and charges > 0:
                 return self.choose_action(state_action)
             else:
                 return self.choose_action(state_action[0:4])
 
         if np.random.uniform() >= self.epsilon_a * self.epsilon + 1:
+            print('Random uniform stuff...')
             if smell and charges > 0:
                 return self.choose_action(state_action)
             else:
                 return self.choose_action(state_action[0:4])
         else:
+            #print('Else...')
             if smell and charges > 0:
                 return np.random.randint(1,9)
             else:
